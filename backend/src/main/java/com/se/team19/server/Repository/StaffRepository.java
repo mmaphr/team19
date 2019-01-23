48e55588e938029bac32246b70854cd565051a3c
mmaphr <aphirach13@hotmail.com>
2019-01-23 21:23:24 +0700

    add StaffRepository - close#73

---
+++ backend/src/main/java/com/se/team19/server/Repository/StaffRepository.java
@@ -0,0 +1,12 @@
+package com.se.team19.server.Repository;
+
+import com.se.team19.server.Entity.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface StaffRepository extends JpaRepository<Staff, Long> {
+    Staff findById(long id);
+}
