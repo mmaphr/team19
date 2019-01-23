cdc4d9b1b2a99d08b2f970dc4cd0e35bfb5a282d
mmaphr <aphirach13@hotmail.com>
2019-01-23 21:07:14 +0700

    edit Entity Gender - close#69

--- backend/src/main/java/com/se/team19/server/Entity/Gender.java
+++ backend/src/main/java/com/se/team19/server/Entity/Gender.java
@@ -1,20 +1,24 @@
 package com.se.team19.server.Entity;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NonNull;
+
 import javax.persistence.*;
-import java.util.Date;
 
 @Data
 @Entity
-@Table(name = "Gender")
+@Table(name = "gender")
 public class Gender {
     @Id
-    @SequenceGenerator(name = "gender_seq", sequenceName = "gender_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gender_seq")
-    @Column(name = "Gender_ID", unique = true, nullable = false)
-    private @NonNull Long id;
-    private @NonNull String gendername;
+    @SequenceGenerator(name="gender_seq",sequenceName="gender_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="gender_seq")
+    @Column(name="GENDER_ID",unique = true, nullable = false)
+    private @NonNull Long GenderId;
+    private @NonNull String GenderName;
 
+    public Gender() {}
 
-
+    public Gender(String genderName) {
+        GenderName = genderName;
+    }
 }
