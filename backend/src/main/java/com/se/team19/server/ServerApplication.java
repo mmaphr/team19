42b3a1c2f6e51c85c1aaab5da862e536a4b922b8
mmaphr <aphirach13@hotmail.com>
2019-01-23 21:39:00 +0700

    Edit ServerApplication - close#75

--- backend/src/main/java/com/se/team19/server/ServerApplication.java
+++ backend/src/main/java/com/se/team19/server/ServerApplication.java
@@ -1,15 +1,13 @@
 package com.se.team19.server;
 
-import com.se.team19.server.Entity.Category;
-import com.se.team19.server.Entity.Note;
-import com.se.team19.server.Repository.AddProductsRepository;
-import com.se.team19.server.Repository.CategoryRepository;
-import com.se.team19.server.Repository.NoteRepository;
+import com.se.team19.server.Entity.*;
+import com.se.team19.server.Repository.*;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -26,10 +24,18 @@ public class ServerApplication {
 		return filter;
 	}
 
-	//<!==========  AddProductToStock ==========!>
+
 	@Bean
-	ApplicationRunner init(CategoryRepository categoryRepository, NoteRepository noteRepository, AddProductsRepository addProductsRepository) {
+	ApplicationRunner init(PositionRepository positionRepository,
+						   ProvinceRepository provinceRepository,
+						   GenderRepository genderRepository,
+						   StaffRepository staffRepository,
+						   CategoryRepository categoryRepository,
+						   NoteRepository noteRepository,
+						   AddProductsRepository addProductsRepository) {
 		return args -> {
+
+			//<!==========  AddProductToStock ==========!>
 			Category category1 = new Category();
 			category1.setCategoryName("อุปกรณ์ทางการแพทย์");
 			categoryRepository.save(category1);
@@ -49,8 +55,44 @@ public class ServerApplication {
 			Note note2 = new Note();
 			note2.setNoteName("ได้รับจากการซื้อ");
 			noteRepository.save(note2);
+
+			//<!========== END!! AddProductToStock ==========!>
+
+
+			//<!==================== Staff ====================!>
+			Stream.of("ชาย", "หญิง").forEach(GenderName -> {
+				genderRepository.save(new Gender(GenderName));
+			});
+			genderRepository.findAll().forEach(System.out::println);
+
+			Stream.of("ฝ่ายบุคคล", "พนักงานทั่วไป").forEach(PositionName -> {
+				positionRepository.save(new Position(PositionName));
+			});
+			positionRepository.findAll().forEach(System.out::println);
+
+			Stream.of("กระบี่","กรุงเทพมหานคร","กาญจนบุรี","กาฬสินธุ์","กำแพงเพชร","ขอนแก่น","จันทบุรี","ฉะเชิงเทรา" ,"ชลบุรี","ชัยนาท","ชัยภูมิ","ชุมพร","เชียงราย","เชียงใหม่","ตรัง","ตราด","ตาก","นครนายก","นครปฐม","นครพนม","นครราชสีมา" ,"นครศรีธรรมราช","นครสวรรค์","นนทบุรี","นราธิวาส","น่าน","บุรีรัมย์","บึงกาฬ","ปทุมธานี","ประจวบคีรีขันธ์","ปราจีนบุรี","ปัตตานี" ,"พะเยา","พังงา","พัทลุง","พิจิตร","พิษณุโลก","เพชรบุรี","เพชรบูรณ์","แพร่","ภูเก็ต","มหาสารคาม","มุกดาหาร","แม่ฮ่องสอน" ,"ยโสธร","ยะลา","ร้อยเอ็ด","ระนอง","ระยอง","ราชบุรี","ลพบุรี","ลำปาง","ลำพูน","เลย","ศรีสะเกษ","สกลนคร","สงขลา" ,"สตูล","สมุทรปราการ","สมุทรสงคราม","สมุทรสาคร","สระแก้ว","สระบุรี","สิงห์บุรี","สุโขทัย","สุพรรณบุรี","สุราษฎร์ธานี" ,"สุรินทร์","หนองคาย","หนองบัวลำภู","อยุธยา","อ่างทอง","อำนาจเจริญ","อุดรธานี","อุตรดิตถ์","อุทัยธานี","อุบลราชธานี").forEach(Provincename -> {
+				provinceRepository.save(new Province(Provincename));
+			});
+			provinceRepository.findAll().forEach(System.out::println);
+
+			Staff S1 = new Staff();
+			S1.setStaffName("SomA");
+			S1.setAge(20);
+			S1.setAddress("123/2");
+			S1.setPhone("081-2345678");
+			S1.setUsername("SomA");
+			S1.setPassword("123456");
+			S1.setStaffGender(genderRepository.findById(1));
+			S1.setStaffProvince(provinceRepository.findById(13));
+			S1.setStaffPosition(positionRepository.findById(2));
+			staffRepository.save(S1);
+			staffRepository.findAll().forEach(System.out::println);
+
+			//<!================= END!! Staff =================!>
+
 		};
 	}
-	//<!========== END!! AddProductToStock ==========!>
+
+
 
 }
