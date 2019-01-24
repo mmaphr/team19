54eafb488e7e135bcd540a4da6062a70796886c3
yongyutsrisuban <yongyut120@hotmail.co.th>
2019-01-24 22:48:50 +0700

    close #88 - ทำ การเพิ่ม Code เกี่ยวกับหน้า Login ใน Entity Staff

--- backend/src/main/java/com/se/team19/server/Entity/Staff.java
+++ backend/src/main/java/com/se/team19/server/Entity/Staff.java
@@ -1,51 +1,41 @@
 package com.se.team19.server.Entity;
 
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.*;
 
-@Entity
 @Data
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
 @Table(name = "staff")
 public class Staff {
     @Id
     @SequenceGenerator(name="staff_seq",sequenceName="staff_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="staff_seq")
     @Column(name ="STAFF_ID",unique = true, nullable = false)
-    private @NonNull Long StaffId;
-    private @NonNull String StaffName;
-    private @NonNull int Age;
-    private @NonNull String Address;
-    private @NonNull String Phone;
-    private @NonNull String Username;
-    private @NonNull String Password;
-
-    public Staff() {}
-
-    public Staff(String staffName, @NonNull int age, String address, String phone, String username, String password, Gender staffGender, Province staffProvince, Position staffPosition) {
-        StaffName = staffName;
-        Age = age;
-        Address = address;
-        Phone = phone;
-        Username = username;
-        Password = password;
-        StaffGender = staffGender;
-        StaffProvince = staffProvince;
-        StaffPosition = staffPosition;
-    }
+    private @NonNull Long staffId;
+    private @NonNull String staffName;
+    private @NonNull int age;
+    private @NonNull String address;
+    private @NonNull String phone;
+    private @NonNull String username;
+    private @NonNull String password;
 
     @ManyToOne()
     @JoinColumn(name = "GENDER_ID", insertable = true)
-    private Gender StaffGender;
+    private Gender staffGender;
 
     @ManyToOne()
     @JoinColumn(name = "PROVINCE_ID", insertable = true)
-    private Province StaffProvince;
+    private Province staffProvince;
 
     @ManyToOne()
     @JoinColumn(name = "POSITION_ID", insertable = true)
-    private Position StaffPosition;
+    private Position staffPosition;
 
 
 }
