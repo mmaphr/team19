0cdc5813c778300d467889f53510815512ec2aff
yongyutsrisuban <yongyut120@hotmail.co.th>
2019-01-25 00:55:14 +0700

    close #99 - ทำ การเพิ่ม code ใน StaffRepository

--- backend/src/main/java/com/se/team19/server/Repository/StaffRepository.java
+++ backend/src/main/java/com/se/team19/server/Repository/StaffRepository.java
@@ -10,4 +10,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "http://localhost:4200")
 public interface StaffRepository extends JpaRepository<Staff, Long> {
    Staff findByUsername(String user);
+   Staff findByUsernameAndPassword(String user,String pass);
 }
