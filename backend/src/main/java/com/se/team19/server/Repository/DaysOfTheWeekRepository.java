14ee361f7e891b3eb9fbc4712338f0276eb3a084
kritsadabr <kritsada.br@gmail.com>
2019-01-24 18:30:51 +0700

    add Repository for ระบบจัดการกิจกรรมภายใน - close#86

---
+++ backend/src/main/java/com/se/team19/server/Repository/DaysOfTheWeekRepository.java
@@ -0,0 +1,11 @@
+package com.se.team19.server.Repository;
+
+import com.se.team19.server.Entity.DaysOfTheWeek;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface DaysOfTheWeekRepository extends JpaRepository<DaysOfTheWeek, Long> {
+
+}
+
---
+++ backend/src/main/java/com/se/team19/server/Repository/TimeDurationRepository.java
@@ -0,0 +1,11 @@
+package com.se.team19.server.Repository;
+
+import com.se.team19.server.Entity.TimeDuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TimeDurationRepository extends JpaRepository<TimeDuration, Long> {
+
+}
+
