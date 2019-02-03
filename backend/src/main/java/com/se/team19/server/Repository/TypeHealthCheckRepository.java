87cce6e95f9ceb9037f361b6d84b70cee3fef9dc
earthrockey <ratchapong5924@gmail.com>
2019-02-04 06:43:48 +0700

    Add TypeHealthCheckRepository - close#149

---
+++ backend/src/main/java/com/se/team19/server/Repository/TypeHealthCheckRepository.java
@@ -0,0 +1,13 @@
+package com.se.team19.server.Repository;
+
+import com.se.team19.server.Entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import java.util.Collection;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface TypeHealthCheckRepository extends JpaRepository<TypeHealthCheck,Long> {
+    TypeHealthCheck findById(long id);
+}
