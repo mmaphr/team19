6f0e256f3c46b4511aac00062022d99b4bf62e1d
mmaphr <aphirach13@hotmail.com>
2019-02-10 00:46:58 +0700

    add TrainAndStaff Repo - close#171

---
+++ backend/src/main/java/com/se/team19/server/Repository/TrainAndStaffRepository.java
@@ -0,0 +1,11 @@
+package com.se.team19.server.Repository;
+
+import com.se.team19.server.Entity.TrainAndStaff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface TrainAndStaffRepository  extends JpaRepository<TrainAndStaff,Long>{
+}
