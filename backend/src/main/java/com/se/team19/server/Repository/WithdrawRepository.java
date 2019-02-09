fc277b97308a0db0f2c8e54d6d6261caad134295
yongyutsrisuban <yongyut120@hotmail.co.th>
2019-02-09 16:49:35 +0700

    close #134 - ทำการสร้าง Repository ของ Entity Withdraw ของระบบเบิกสินค้า

---
+++ backend/src/main/java/com/se/team19/server/Repository/WithdrawRepository.java
@@ -0,0 +1,14 @@
+package com.se.team19.server.Repository;
+
+import com.se.team19.server.Entity.Withdraw;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Collection;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface WithdrawRepository extends JpaRepository<Withdraw,Long> {
+    Collection<Withdraw> findByWithdrawId(long l);
+}
