d0e4a3e155244d7128d3bd4aff315528accd74f0
yongyutsrisuban <yongyut120@hotmail.co.th>
2019-02-09 16:53:36 +0700

    close #135 - ทำการสร้าง DepartmentRepository ของระบบเบิกสินค้า

---
+++ backend/src/main/java/com/se/team19/server/Repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package com.se.team19.server.Repository;
+
+import com.se.team19.server.Entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+    Department findByDepartmentName(String s);
+}
