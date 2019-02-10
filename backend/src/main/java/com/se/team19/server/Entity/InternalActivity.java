858e16ccd19293973e7e42a5b0f1db337267cdd5
kritsadabr <kritsada.br@gmail.com>
2019-02-10 11:00:10 +0700

    edit InternalActivity.java - close#200

--- backend/src/main/java/com/se/team19/server/Entity/InternalActivity.java
+++ backend/src/main/java/com/se/team19/server/Entity/InternalActivity.java
@@ -3,37 +3,44 @@ package com.se.team19.server.Entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Data
 @Entity
-@NoArgsConstructor
 @Table(uniqueConstraints = {
         @UniqueConstraint(columnNames = {"dayId", "timeId"})
 })
 public class InternalActivity {
     @Id
     @GeneratedValue(generator = "InternalActivity_Seq")
-    private @NonNull Long actI;
-    private @NonNull String actName;
+    private @NotNull Long actId;
+    @Pattern(regexp = "[A-Za-zก-์' ']+$")
+    @Size(min = 5, max = 30)
+    private @NotNull String actName;
+    @Size(max = 50)
     private String description;
 
     @ManyToOne
     @JoinColumn(name = "staffId")
-    private Staff staff;
+    private @NotNull Staff staff;
 
     @ManyToOne
     @JoinColumn(name = "dayId")
-    private DaysOfTheWeek day;
+    private @NotNull DaysOfTheWeek day;
 
     @ManyToOne
     @JoinColumn(name = "timeId")
-    private TimeDuration time;
+    private @NotNull TimeDuration time;
 
-    public InternalActivity(@NonNull String actName, String description, Staff staff, DaysOfTheWeek day, TimeDuration time) {
+    public InternalActivity() {
+    }
+
+    public InternalActivity(String actName, String description, Staff staff, DaysOfTheWeek day, TimeDuration time) {
         this.actName = actName;
         this.description = description;
         this.staff = staff;
         this.day = day;
         this.time = time;
     }
+
 }
